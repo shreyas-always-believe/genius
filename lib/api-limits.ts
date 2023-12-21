cf543f688263f4ade5463705824bca37c17c9767
Shreyas Chaudhari <shreyas.chaudhari@n26.com>
2023-12-21 08:09:28 +0100

    Fix for Prisma is not defined error

--- lib/api-limits.ts
+++ lib/api-limits.ts
@@ -1,7 +1,7 @@
 import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
-import { MAX_FREE_COUNTS } from "@/prisma/constants";
+import { MAX_FREE_COUNTS } from "@/constants";
 
 export const increaseApiLimit = async () => {
   const { userId } = auth();
